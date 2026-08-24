1cf85d0135cedfc9b5055411a07dc2c81a2b8c27
oswin-m4a <132178516+time1043@users.noreply.github.com>
2026-08-24 23:55:34 +0800

    chore: learn expo-router, stack, and navigate (2/x)

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,15 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const router = useRouter();
+
   return (
     <View style={styles.container}>
       <Text>Edit src/app/index.tsx to edit this screen.</Text>
+
+      {/* <Link href="/about">About</Link> */}
+      <Text onPress={() => router.navigate("/about")}>About</Text>
     </View>
   );
 }
