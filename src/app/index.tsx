0a42ae912ca95d1d022265065137b0f4eb9e8adb
oswin-m4a <132178516+time1043@users.noreply.github.com>
2026-08-26 17:09:33 +0800

    chore: configure native-wind in react-native - version: `tailwind@3`, `pnpm@11` (4/x)
    https://www.nativewind.dev/docs/getting-started/installation

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,12 @@
 import { useRouter } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   const router = useRouter();
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.hint}>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-xl text-gray-400">
         Edit src/app/index.tsx to edit this screen.
       </Text>
 
@@ -15,16 +15,3 @@ export default function Index() {
     </View>
   );
 }
-
-// It is like css-modules / css-in-js in react-web
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  hint: {
-    fontSize: 16,
-    color: "#888",
-  },
-});
