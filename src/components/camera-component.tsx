e4cbc63ebff7b16a646e04c934ab0a308780bcbd
oswin-m4a <132178516+time1043@users.noreply.github.com>
2026-09-02 22:33:18 +0800

    feat: implement ui of taking photo (12/x; 3)

--- src/components/camera-component.tsx
+++ src/components/camera-component.tsx
@@ -1,11 +1,25 @@
+import { Host, Icon } from "@expo/ui";
 import { CameraType, CameraView, useCameraPermissions } from "expo-camera";
-import { useState } from "react";
+import { cssInterop } from "nativewind";
+import { ComponentProps, ComponentType, RefAttributes, useState } from "react";
 import { Button, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+type StyledCameraViewProps = ComponentProps<typeof CameraView> & {
+  className?: string;
+} & RefAttributes<CameraView>;
+
+const StyledCameraView = cssInterop(CameraView, {
+  className: "style",
+}) as ComponentType<StyledCameraViewProps>;
 
 export default function CameraComponent() {
   const [facing, setFacing] = useState<CameraType>("back");
   const [permission, requestPermission] = useCameraPermissions();
+
+  const [isCameraReady, setIsCameraReady] = useState(false);
+  const [isTakingPhoto, setIsTakingPhoto] = useState(false);
+  const isCameraDisabled = !isCameraReady || isTakingPhoto;
 
   // Camera permissions are still loading.
   if (!permission) {
@@ -28,16 +42,42 @@ export default function CameraComponent() {
     setFacing((current) => (current === "back" ? "front" : "back"));
   }
 
+  async function handleTakePhoto() {}
+
   return (
     <SafeAreaView className="flex-1 justify-center">
-      <CameraView style={{ flex: 1 }} facing={facing} />
+      {/* Original CameraView is not support to className */}
+      <StyledCameraView
+        className="flex-1"
+        facing={facing}
+        onCameraReady={() => setIsCameraReady(true)}
+      />
 
-      <View className="absolute bottom-16 flex-row bg-transparent w-full px-16">
+      {/* Camera switch button */}
+      <TouchableOpacity
+        className="absolute top-16 right-4 rounded-full bg-black/50 px-4 py-2"
+        onPress={toggleCameraFacing}
+      >
+        <Text className="font-semibold text-white">Flip Camera</Text>
+      </TouchableOpacity>
+
+      {/* Take photo button */}
+      <View className="absolute bottom-8 w-full items-center">
         <TouchableOpacity
-          className="flex-1 items-center"
-          onPress={toggleCameraFacing}
+          className="items-center justify-center border-4 border-white bg-black/30 p-1 size-20 rounded-full"
+          onPress={handleTakePhoto}
+          disabled={isCameraDisabled}
         >
-          <Text className="text-2xl font-bold text-white">Flip Camera</Text>
+          <Host matchContents pointerEvents="none">
+            <Icon
+              name={Icon.select({
+                ios: "camera.fill",
+                android: import("@expo/material-symbols/photo_camera.xml"),
+              })}
+              size={32}
+              color="white"
+            />
+          </Host>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
